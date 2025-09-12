73229596709f17e2cc15d9775341e9038cd3cf6f
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2025-09-12 23:30:06 +0530

    Add files via upload

---
+++ Datatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char cvalue = 'S';
+    int ivalue = 243;
+    double dvalue= 23.1234;
+    float fvalue=12.21;
+
+
+    printf("%c \n", cvalue);
+    printf("%d \n", ivalue);
+    printf("%f \n", fvalue);
+    printf("%lf \n", dvalue);
+
+    return 0;
+};
