aaf7e66de517e8d77e5e49c0b00dbe8b0107b70e
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2025-09-19 13:53:07 +0530

    Add files via upload

---
+++ modifierX.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    int j=-20;
+    int k=-10;
+
+    signed int x=10;
+    signed int y=-10;
+    signed int z=+10;
+
+    unsigned int p=10;
+    unsigned int q=30;
+    unsigned int r=20;
+
+
+    printf("%d \n",p);
+    printf("%d \n",q);
+    printf("%d \n",r);
+
+
+
+    return 0;
+}
