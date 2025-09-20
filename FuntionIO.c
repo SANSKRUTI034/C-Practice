7792b35c50aff9d24a2fa4823147e21de1de4030
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2025-09-20 23:32:53 +0530

    Add files via upload

---
+++ FuntionIO.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int ivalue1= 0 ,  ivalue2= 0, iANS= 0;
+
+    printf("enter your first number: \n");
+    scanf("%d",&ivalue1);
+
+    printf("enter your second number: \n");
+    scanf("%d",&ivalue2);
+
+
+    iANS= ivalue1 +ivalue2 ;
+    printf("the addition is : %d \n" , iANS);
+
+
+
+
+
+
+
+    return 0;
+}
