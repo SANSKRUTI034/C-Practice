9f59d128684e84368a12d52449ec612633e54a4e
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2025-09-22 15:27:54 +0530

    Add files via upload

---
+++ FunctionDemo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int value1 = 0 , value2 = 0 , ans=0;
+
+    value1 = 11;
+    value2 = 21;
+
+    ans = value1 + value2;
+
+    printf("The answer is : %d \n" , ans);
+
+
+
+
+
+
+    return 0;
+}
