f9295670b07b7a595f7470250b4550af5f71984f
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2025-09-28 20:33:47 +0530

    Add files via upload

---
+++ ArrayDemo2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int Arr[5]={10,20,30,40,50};
+
+    int Brr[3];
+    Brr[0]=50;
+    Brr[1]=60;
+    Brr[2]=70;
+
+
+    printf("%d\n",Arr[4]);
+    printf("%d\n",Brr[2]);
+    printf("%d\n",Arr[0]);
+    printf("The size of Brr is : %d \n",Arr[5]);
+
+
+
+
+
+    return 0;
+}
