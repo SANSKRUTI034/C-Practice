3fa37ff9b5a642fa0c77bcd46a82e0c2c44b99ee
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2025-09-28 20:32:58 +0530

    Add files via upload

---
+++ ArrayDemo1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int Arr[4]= {10,20,30,40};
+    int Brr[5]= {11,22,33,44,55};
+    int Crr[3]= {1,2,3,};
+
+
+    printf("The size of Arr is : %lu \n", sizeof(Arr));
+    printf("The size of Brr is : %lu \n", sizeof(Brr));
+    printf("The size of Crr  is : %lu \n", sizeof(Crr));
+
+
+
+    return 0;
+}
