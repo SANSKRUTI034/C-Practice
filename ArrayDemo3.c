1274f79b3fd9faadb94b9932cf68f531e1b3746e
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2025-09-30 22:17:39 +0530

    Add files via upload

---
+++ ArrayDemo3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int Arr[5]={10,20,30,40,50};
+
+
+    printf("Arr: %d\n" , Arr);
+    printf("&Arr: %d\n" , &Arr);
+    printf("&Arr[3]: %d\n" , &Arr[3]);
+
+
+
+
+
+    
+    
+    
+    return 0;
+}
